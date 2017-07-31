9f8b74242c51901a61c948920b13ad6fa0ad7ffc
Nicky Kimaina <nkimaina@ampath.or.ke>
2017-07-31 09:19:52 +0300

    NGPOC-412: Added resource method to fetch visit type per patient per program

--- src/app/etl-api/patient-program-resource.service.ts
+++ src/app/etl-api/patient-program-resource.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, URLSearchParams, Response } from '@angular/http';
 import { Observable, Subject } from 'rxjs/Rx';
 import { AppSettingsService } from '../app-settings/app-settings.service';
+import { Program } from '../models/program.model';
 
 
 @Injectable()
@@ -22,6 +23,30 @@ export class PatientProgramResourceService {
     let url = this.appSettingsService.getEtlRestbaseurl().trim()
       + 'patient-program/' + patientUuid + '/program/' + programUuid;
     return this.http.get(url).map((response: Response) => {
+      return response.json();
+    });
+  }
+
+  /**
+   *
+   *
+   * @param {string} patientUuid patient uuid
+   * @param {string} programUuid program uuid
+   * @param {any} enrollmentUuid patient program enrollment uuid
+   * @param {string} locationUuid the intended location where one wants to start the visits
+   * @returns {Observable<any>} returns a programs configrations opbject that contains the visit
+   * that can be started in that location
+   * @memberof PatientProgramResourceService
+   */
+  getPatientProgramVisitTypes(patientUuid: string, programUuid: string,
+    enrollmentUuid, locationUuid: string): Observable<any> {
+    let url = this.appSettingsService.getEtlRestbaseurl().trim()
+      + 'patient/' + patientUuid + '/program/' + programUuid +
+      '/enrollment/' + enrollmentUuid;
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('intendedLocationUuid', (locationUuid && locationUuid.length > 0)
+      ? locationUuid : locationUuid);
+    return this.http.get(url, { search: params }).map((response: Response) => {
       return response.json();
     });
   }
